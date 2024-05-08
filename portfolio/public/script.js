f6539420e273b64435af2f82b7fda4a596e8ad1f
Engr-Samson-Beloved <125021538+Engr-Samson-Beloved@users.noreply.github.com>
2024-05-09 00:07:43 +0100

    Add files via upload

---
+++ portfolio/public/script.js
@@ -0,0 +1,8 @@
+// function toggleMenu() {
+//     document.getElementById("navbar-menu").style.left = "0px";
+
+//   };
+let dropdown = document.getElementById("navbar-menu")
+ let nav = document.getElementById("nav").addEventListener(click, ()=>{
+  dropdown.style.left = "0px";
+ })
